0845976097fc2dec9ffd862511ab3ecf669c65e5
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-15 18:58:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE gender = 'F';"
+  "SELECT bears.names, bears.age FROM bears WHERE bear.gender == "f";" 
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
